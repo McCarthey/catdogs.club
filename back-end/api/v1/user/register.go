87d67c0f4c9c0d637bf65a3a7ad90428ca259ec0
Yoko <18836617@qq.com>
2019-04-17 16:54:53 +0800

    [feat] 保存邮箱code

--- back-end/api/v1/user/register.go
+++ back-end/api/v1/user/register.go
@@ -17,23 +17,34 @@ func Register(c *gin.Context) {
 	if err != nil {
 		fmt.Println(err)
 	}
+
 	has := verifyUser(&user)
 	if has {
 		libs.Resp(c, -1000, "用户已存在", gin.H{})
 		return
 	}
-	err = saveUser(&user)
-	if err != nil {
-		fmt.Println(err)
-	}
 	code := libs.RandString(6)
 	cont := fmt.Sprintf("您的邮箱验证码为: %s", code)
+	go saveUser(&user)
+	go saveCode(code, &user)
 	go libs.SendMail(user.Email, "邮箱验证", cont)
 	libs.Resp(c, 0, "success", gin.H{})
 }
 
+func saveCode(code string, param *User) {
+	v := models.VerifyCode{
+		Email:     param.Email,
+		Code:      code,
+		Timestamp: int(time.Now().Unix()),
+	}
+	err := v.Set()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
 // 保存用户到数据库
-func saveUser(param *User) error {
+func saveUser(param *User) {
 	pwData := md5.Sum([]byte(param.Password))
 	pwS := fmt.Sprintf("%x", pwData)
 	u := models.User{
@@ -42,7 +53,9 @@ func saveUser(param *User) error {
 		RegisterTime: int(time.Now().Unix()),
 	}
 	err := u.Set()
-	return err
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 // 验证用户是否存在
--- back-end/libs/common.go
+++ back-end/libs/common.go
@@ -1,15 +1,22 @@
 package libs
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 const (
 	letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 )
 
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
 func RandString(n int) string {
 	b := make([]byte, n)
 	for i := range b {
-		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+		b[i] = letterBytes[rand.Int63()%int64(len(letterBytes))]
 	}
 	return string(b)
 }
