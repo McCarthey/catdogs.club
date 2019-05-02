014b263465aeb24b026a1742f57111343f7615a1
Yoko <18836617@qq.com>
2019-05-02 13:49:46 +0800

    [init] jwt lib

---
+++ back-end/libs/jwt.go
@@ -0,0 +1,45 @@
+package libs
+
+import (
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var jwtSecret = []byte("catdogs===")
+
+type Claims struct {
+	Id int `json:"id"`
+	jwt.StandardClaims
+}
+
+func GenerateToken(id int) (string, error) {
+	nowTime := time.Now()
+	expireTime := nowTime.Add(24 * time.Hour)
+	claims := Claims{
+		id,
+		jwt.StandardClaims{
+			ExpiresAt: expireTime.Unix(),
+			Issuer:    "catdogs",
+		},
+	}
+
+	tokenClaims := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token, err := tokenClaims.SignedString(jwtSecret)
+
+	return token, err
+}
+
+func ParseToken(token string) (*Claims, error) {
+	tokenClaims, err := jwt.ParseWithClaims(token, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	})
+
+	if tokenClaims != nil {
+		if claims, ok := tokenClaims.Claims.(*Claims); ok && tokenClaims.Valid {
+			return claims, nil
+		}
+	}
+
+	return nil, err
+}
