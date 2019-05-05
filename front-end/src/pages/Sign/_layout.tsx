bc19658d1fa0df29be28d818da31e273bd10fc93
Randall <randall@duomai.com>
2019-05-05 17:32:41 +0800

    [add] routes animation

--- front-end/src/pages/Sign/_layout.tsx
+++ front-end/src/pages/Sign/_layout.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
 export default function Sign(props: any) {
-	return (
-		<div>
-			Sign
-			{props.children}
-		</div>
-	)
+    return (
+        <div>
+            <TransitionGroup exit={false}>
+                <CSSTransition key={location.pathname} classNames="fade" timeout={300}>
+                    <div style={{ background: '#fff', padding: 24}}>
+                        {props.children}
+                    </div>
+                </CSSTransition>
+            </TransitionGroup>
+        </div>
+    )
 }
