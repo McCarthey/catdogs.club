9ac45ef52c7b169f1060839adcf4e2d03c1ec266
Yoko <18836617@qq.com>
2019-05-20 10:19:01 +0800

    [fix] verify query

--- back-end/api/v1/user/verify.go
+++ back-end/api/v1/user/verify.go
@@ -1,34 +1,30 @@
 package user
 
 import (
+	"net/http"
+
 	"catdogs.club/back-end/logging"
 	"catdogs.club/back-end/models"
 	"github.com/gin-gonic/gin"
 )
 
-type VParam struct {
-	V string `form:"v"`
-}
-
 func Verify(c *gin.Context) {
-	var vparam VParam
-	err := c.ShouldBind(vparam)
+	v := c.Query("v")
+	vm := models.VerifyCode{
+		Code: v,
+	}
+	has, err := vm.Get()
 	if err != nil {
-		logging.Error(err)
+		logging.Error("vm get error: ", err)
 	}
-	logging.Info("v param", vparam)
-	v := models.VerifyCode{
-		Code: vparam.V,
-	}
-	has, err := v.Get()
 	logging.Info("verify code: ", v)
 	if has {
 		sql := "update user set flags=flags|? where email=?;"
-		_, err := models.Db.Exec(sql, models.IsActivate, v.Email)
+		_, err := models.Db.Exec(sql, models.IsActivate, vm.Email)
 		if err != nil {
-			c.String(0, err.Error(), "")
+			c.String(0, err.Error())
 			return
 		}
 	}
-	c.String(0, "账号激活成功", "")
+	c.String(http.StatusOK, "账号激活成功\n")
 }
