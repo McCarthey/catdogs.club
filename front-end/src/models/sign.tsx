dde5c6ff3d305e96d10c27795d4f64bcff0144d0
Randall <randall@duomai.com>
2019-05-24 13:57:22 +0800

    [add] signup with dva

---
+++ front-end/src/models/sign.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import api_sign from '../services/sign'
+import { SignUpReq } from '@/types/sign'
+import { DvaEffect } from '@/types/common'
+import { message, Modal } from 'antd'
+import router from 'umi/router'
+
+
+interface GlobalModelState {
+    isLoggedIn: boolean
+}
+
+interface LoginAction {
+    payload: {
+        isLoggedIn: boolean
+    }
+}
+
+export default {
+    state: {
+        isLoggedIn: false,
+    } as GlobalModelState,
+    reducers: {
+        changeLogState(state: GlobalModelState, { payload: { isLoggedIn } }: LoginAction) {
+            return { ...state, isLoggedIn }
+        },
+    },
+    effects: {
+        *signUp({ payload: { email, password }}: { payload: SignUpReq }, { call, put }: DvaEffect) {
+            const res = yield call(api_sign.signUpByEmail, { email, password })
+            console.log(res)
+            Modal.success({
+                title: '注册成功',
+                content: (
+                    <div>
+                        请前往
+                        <span style={{ fontWeight: 'bold' }}>{email}</span>
+                        查看收件箱，激活账号
+                    </div>
+                ),
+                onOk() {
+                    router.push('/sign/signin')
+                },
+            })
+        },
+        *login({ payload: { email, password }}: { payload: SignUpReq }, { call, put }: DvaEffect) {
+            const res = yield call(api_sign.signInByEmail, { email, password })
+            yield put({ type: 'changeLogState', payload: { isLoggedIn: true } })
+            message.success(res.msg)
+            router.push('/home')
+        },
+        *logout() {
+            yield // TODO:暂无推出登录接口
+        }
+    },
+}
