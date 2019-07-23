6519c21c6d6587c72aef12229cfe38e35f376c90
Yoko <18836617@qq.com>
2019-07-23 13:16:36 +0800

    [add] setpost doc

--- back-end/api/v1/post/setpost.go
+++ back-end/api/v1/post/setpost.go
@@ -1,8 +1,6 @@
 package post
 
 import (
-	"fmt"
-
 	client "catdogs.club/back-end/client/post"
 	"catdogs.club/back-end/libs"
 	"catdogs.club/back-end/logging"
@@ -10,10 +8,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @Tags 文章
+// @Summary 发布文章
+// @Description 发布文章
+// @Accept  json
+// @Produce  json
+// @Param title formData string true "标题"
+// @Param content formData string true "内容"
+// @Param author formData string true "作者"
+// @Success 0 {string} string "{"code": 0, "data": {}, "msg": "success"}"
+// @Failure -999 {string} string "服务器出问题"
+// @Failure -3000 {string} string "参数错误"
+// @Router /setpost [post]
 func SetPostHandler(c *gin.Context) {
 	var setpost SetPost
 	if err := c.ShouldBind(&setpost); err != nil {
-		fmt.Println(err)
 		logging.Error("bind setpost err: ", err)
 		libs.Resp(libs.R{
 			C:    c,
